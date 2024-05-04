f7b25093a7a3a18394f5e30a617af701c6a9bcaf
YassineVos <yessinboukrach@hotmail.com>
2024-05-04 21:16:14 +0200

    Refactor user.controller.js to use add instead of create method

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,73 +1,70 @@
-const userService = require('../services/user.service')
+const { add } = require("../dao/inmem-db");
+const userService = require("../services/user.service");
 
 let userController = {
-    create: (req, res, next) => {
-        const user = req.body
-        //
-        // Todo: Validate user input
-        //
-        userService.create(user, (error, success) => {
-            if (error) {
-                return next({
-                    status: error.status,
-                    message: error.message,
-                    data: {}
-                })
-            }
-            if (success) {
-                res.status(200).json({
-                    status: success.status,
-                    message: success.message,
-                    data: success.data
-                })
-            }
-        })
-    },
+  add: (req, res, next) => {
+    const user = req.body;
+    //
+    // Todo: Validate user input
+    //
+    userService.add(user, (error, success) => {
+      if (error) {
+        return next({
+          status: error.status,
+          message: error.message,
+          data: {},
+        });
+      }
+      if (success) {
+        res.status(200).json({
+          status: success.status,
+          message: success.message,
+          data: success.data,
+        });
+      }
+    });
+  },
 
-    getAll: (req, res, next) => {
-        userService.getAll((error, success) => {
-            if (error) {
-                return next({
-                    status: error.status,
-                    message: error.message,
-                    data: {}
-                })
-            }
-            if (success) {
-                res.status(200).json({
-                    status: 200,
-                    message: success.message,
-                    data: success.data
-                })
-            }
-        })
-    },
+  getAll: (req, res, next) => {
+    userService.getAll((error, success) => {
+      if (error) {
+        return next({
+          status: error.status,
+          message: error.message,
+          data: {},
+        });
+      }
+      if (success) {
+        res.status(200).json({
+          status: 200,
+          message: success.message,
+          data: success.data,
+        });
+      }
+    });
+  },
 
-    getById: (req, res, next) => {
-        const userId = req.params.userId
-        userService.getById(userId, (error, success) => {
-            if (error) {
-                return next({
-                    status: error.status,
-                    message: error.message,
-                    data: {}
-                })
-            }
-            if (success) {
-                res.status(200).json({
-                    status: success.status,
-                    message: success.message,
-                    data: success.data
-                })
-            }
-        })
-    }
+  getById: (req, res, next) => {
+    const userId = req.params.userId;
+    userService.getById(userId, (error, success) => {
+      if (error) {
+        return next({
+          status: error.status,
+          message: error.message,
+          data: {},
+        });
+      }
+      if (success) {
+        res.status(200).json({
+          status: success.status,
+          message: success.message,
+          data: success.data,
+        });
+      }
+    });
+  },
 
+  // Todo: Implement the update and delete methods
+};
 
-    
-
-
-    // Todo: Implement the update and delete methods
-}
-
-module.exports = userController
+module.exports = userController;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/user.controller')
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user.controller");
 
 // Tijdelijke functie om niet bestaande routes op te vangen
 const notFound = (req, res, next) => {
-    res.status(404).json({
-        status: 404,
-        message: 'Route not found',
-        data: {}
-    })
-}
+  res.status(404).json({
+    status: 404,
+    message: "Route not found",
+    data: {},
+  });
+};
 
 // Userroutes
-router.post('/api/users', userController.create)
-router.get('/api/users', userController.getAll)
-router.get('/api/users/:userId', userController.getById)
+router.post("/api/users", userController.add);
+router.get("/api/users", userController.getAll);
+router.get("/api/users/:userId", userController.getById);
 
 // Tijdelijke routes om niet bestaande routes op te vangen
-router.put('/api/users/:userId', notFound)
-router.delete('/api/users/:userId', notFound)
+router.put("/api/users/:userId", notFound);
+router.delete("/api/users/:userId", notFound);
 
-module.exports = router
+module.exports = router;

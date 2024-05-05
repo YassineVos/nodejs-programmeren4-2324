1f16083cc0e45aad84541e88451c094a3b558613
YassineVos <yessinboukrach@hotmail.com>
2024-05-05 14:20:29 +0200

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,28 +2,28 @@ const { add } = require("../dao/inmem-db");
 const userService = require("../services/user.service");
 
 let userController = {
-    create: (req, res, next) => {
-        const user = req.body
-        //
-        // Todo: Validate user input
-        //
-        userService.create(user, (error, success) => {
-            if (error) {
-                return next({
-                    status: error.status,
-                    message: error.message,
-                    data: {}
-                })
-            }
-            if (success) {
-                res.status(200).json({
-                    status: success.status,
-                    message: success.message,
-                    data: success.data
-                })
-            }
-        })
-    },
+  add: (req, res, next) => {
+    const user = req.body;
+    //
+    // Todo: Validate user input
+    //
+    userService.create(user, (error, success) => {
+      if (error) {
+        return next({
+          status: error.status,
+          message: error.message,
+          data: {},
+        });
+      }
+      if (success) {
+        res.status(200).json({
+          status: success.status,
+          message: success.message,
+          data: success.data,
+        });
+      }
+    });
+  },
 
   getAll: (req, res, next) => {
     userService.getAll((error, success) => {
@@ -44,25 +44,25 @@ let userController = {
     });
   },
 
-    getById: (req, res, next) => {
-        const userId = req.params.userId
-        userService.getById(userId, (error, success) => {
-            if (error) {
-                return next({
-                    status: error.status,
-                    message: error.message,
-                    data: {}
-                })
-            }
-            if (success) {
-                res.status(200).json({
-                    status: success.status,
-                    message: success.message,
-                    data: success.data
-                })
-            }
-        })
-    }
+  getById: (req, res, next) => {
+    const userId = req.params.userId;
+    userService.getById(userId, (error, success) => {
+      if (error) {
+        return next({
+          status: error.status,
+          message: error.message,
+          data: {},
+        });
+      }
+      if (success) {
+        res.status(200).json({
+          status: success.status,
+          message: success.message,
+          data: success.data,
+        });
+      }
+    });
+  },
 
   // Todo: Implement the update and delete methods
 };

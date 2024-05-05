db6a2f323eda1195f511d64334ab8abbe79b0e4f
YassineVos <yessinboukrach@hotmail.com>
2024-05-05 11:18:27 +0200

    Add input validation to user creation route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -73,7 +73,7 @@ const validateUserCreateChaiExpect = (req, res, next) => {
 };
 
 // Userroutes
-router.post("/api/users", userController.add);
+router.post("/api/users", validateUserCreateChaiShould, userController.add);
 router.get("/api/users", userController.getAll);
 router.get("/api/users/:userId", userController.getById);
 

a42c85469dd8f4ed1c0a0fd39ce1a9b4d50cccd0
YassineVos <yessinboukrach@hotmail.com>
2024-05-29 10:56:32 +0200

    chore: Update database configuration for test environment

--- mysql-pool-example.js
+++ mysql-pool-example.js
@@ -14,7 +14,7 @@ const dbConfig = {
   port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "share-a-meal",
+  database: process.env.DB_DATABASE || "share-a-meal-testdb",
   multipleStatements: true,
 };
 

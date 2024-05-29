00033f38c9a2e4473d1f599916e09b0207004413
YassineVos <yessinboukrach@hotmail.com>
2024-05-29 19:49:28 +0200

    chore: Update server port to use environment variable

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use("/api", userRoutes);
 
 app.use(validateToken);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get("/", function (req, res) {
   res.json({ message: "Hello World" });

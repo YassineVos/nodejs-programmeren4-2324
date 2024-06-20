d72588ce6736c0d68f5c3422cae6eff920874e65
YassineVos <yessinboukrach@hotmail.com>
2024-06-20 18:24:39 +0200

    chore: Update PORT variable to default to 25060 if not set in environment

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get("/api/info", (req, res) => {
 
 app.use(validateToken);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 25060;
 
 app.get("/", function (req, res) {
   res.json({ message: "Hello World" });
